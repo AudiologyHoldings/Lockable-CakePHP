0882fb0fc538a17ed1f8cc2d2830df95ca7f22f6
Matt Reishus <matt.reishus@audiologyholdings.com>
2014-08-21 17:08:36 -0500

    Remove redis locking (It didn't work)

--- Model/Behavior/LockableBehavior.php
+++ Model/Behavior/LockableBehavior.php
@@ -11,19 +11,16 @@ if (!class_exists('LockException')) {
 App::uses('ModelBehavior', 'Model');
 class LockableBehavior extends ModelBehavior {
 	public $settings  = [];
-	public $locks     = [];
 	public $mutexes   = [];
 	public $mutexInts = [];
 
 	// What is the current lock locked via?
-	//    possible values:  false (no lock), 'mutex', or 'redis
+	//    possible values:  false (no lock), 'mutex'
 	protected $_currentLockMethod = [];
 
 	protected $_defaults = [
 		// lock with Mutex (current machine only, multi-process locked)
 		'mutex' => true,
-		// lock with Redis (multiple machines, multiple redisServers possible
-		'redis' => true,
 		// Mutex requires an int ID,
 		//   we need to "prefix" the ints to make them unique to this Model
 		//   this "prefix" should remain consistant on this Model
@@ -32,36 +29,12 @@ class LockableBehavior extends ModelBehavior {
 		//   if > 0, we modulus the mutexInt by this
 		//   so that we only have up to X number of buckets per Model
 		'mutexIntBuckets' => 100,
-		// Redis Servers to use with RedLock
-		//   if empty, we look through all Caches configured for RedisEngine
-		//   example:
-		//     array(
-		//       array('localhost', 6379, 0.01),
-		//       array('localhost', 6378, 0.01),
-		//       array('localhost', 6377, 0.01),
-		//     )
-		'redisServers' => array(),
-		// Redis can Rety X times...
-		//   if you want to BLOCK for a  long time before timeout, set this high [100]
-		//   if you want to BLOCK for a short time before timeout, set this low  [3]
-		'redisRetryCount' => 100,
-		// Redis Retry attempts will be delayed by X milliseconds
-		'redisRetryDelay' => 200,
-		// Redis Locks need a redisTTL, a nmumber of milliseconds to live for
-		//   after this TTL, they auto-unlock
-		//     60000 = 60 seconds = 1 min
-		'redisTTL' => 60000,
 	];
 
 	/**
 	 * Placeholder... did we ever run Mutex setup (for any model)
 	 */
 	protected $_mutex_ran_setup = false;
-
-	/**
-	 * Placeholder... did we ever run Redis setup (for any model)
-	 */
-	protected $_redis_ran_setup = false;
 
 	/**
 	 * Configure the behavior through the Model::actsAs property
@@ -71,9 +44,7 @@ class LockableBehavior extends ModelBehavior {
 	 */
 	public function setup(Model $Model, $config = null) {
 		// Ints    - Numeric keys used to identify mutex objects, indexed by model $id
-		// Locks   - Redis lock objects, indexed by model $id
 		// Mutexes - Mutex resources, indexed by "ints" or the numeric keys two lines above
-		$this->locks[$Model->alias]   = array();
 		$this->mutexes[$Model->alias] = array();
 		$this->mutexInts[$Model->alias]    = array();
 
@@ -102,11 +73,6 @@ class LockableBehavior extends ModelBehavior {
 		if (!is_numeric($this->settings[$Model->alias]['mutexIntPrefix'])) {
 			throw new LockException('Lockable Setup Problem - mutexIntPrefix must be an integer.');
 		}
-
-		// if redisTTL < 1, set to 10
-		if (empty($this->settings[$Model->alias]['redisTTL']) || (int)$this->settings[$Model->alias]['redisTTL'] < 10) {
-			$this->settings[$Model->alias]['redisTTL'] = 10;
-		}
 	}
 
 	/**
@@ -128,33 +94,8 @@ class LockableBehavior extends ModelBehavior {
 	}
 
 	/**
-	 * Runs on first try of locking via Redis.
-	 * Attempts to set up a RedLock object.
-	 *
-	 * If failed, $setting['redis'] is toggled to false and an error is logged.
-	 *
-	 * @param object $Model
-	 * @return boolean
-	 */
-	public function setupRedis(Model $Model) {
-		$error = '';
-		try {
-			$RedLock = $this->getRedLockObject($Model);
-		} catch (Exception $e) {
-			$error = $e->getMessage();
-		}
-		if (!empty($RedLock) && is_object($RedLock)) {
-			return true;
-		}
-		$this->settings[$Model->alias]['redis'] = false;
-		$this->log("LockableBehavior: Exception [%s].  Redis Lock disabled. $error", 'error');
-		return false;
-	}
-
-	/**
 	 * secure this function against running twice at once
 	 *  - pass 1, use a mutext if we can
-	 *  - pass 2, use a redis lock if we can
 	 *
 	 * @param Model $Model
 	 * @param mixed $id
@@ -162,25 +103,6 @@ class LockableBehavior extends ModelBehavior {
 	 * @throws LockException if no locking system available
 	 */
 	public function lock(Model $Model, $id) {
-		// If allowed, try redis lock and return true on success
-		if ($this->settings[$Model->alias]['redis']) {
-			try {
-				if ($this->lockRedis($Model, $id)) {
-					$this->_setCurrentLockMethod($Model, $id, 'redis');
-					return true;
-				}
-			} catch (Exception $e) {
-				if (class_exists('AppLog')) {
-					AppLog::error('Exception caught while trying to lock Redis: ' . $e->getMessage());
-				}
-				if (!$this->settings[$Model->alias]['mutex']) {
-					throw new LockException('LockableBehavior:  Unable to lock with redis. Got exception:' . $e->getMessage());
-				} else {
-					// Falling back to mutex.
-				}
-			}
-		}
-
 		// If allowed, try mutex lock and return true on success
 		if ($this->settings[$Model->alias]['mutex']) {
 			if ($this->lockMutex($Model, $id)) {
@@ -189,7 +111,7 @@ class LockableBehavior extends ModelBehavior {
 			}
 		}
 
-		throw new LockException('LockableBehavior:  Unable to lock with either redis or mutex method.');
+		throw new LockException('LockableBehavior:  Unable to lock with mutex method.');
 	}
 
 	/**
@@ -259,46 +181,6 @@ class LockableBehavior extends ModelBehavior {
 	}
 
 	/**
-	 * We'd like out locking to be consistant across multiple application servers...
-	 * the easiest way is to set it up in Redis with a datetime stamp
-	 *
-	 * this is a "double-check" which should be done after the semaphore has been aquired
-	 *
-	 * @param Model $Model
-	 * @param mixed $id
-	 * @return boolean
-	 */
-	public function lockRedis(Model $Model, $id) {
-		if (empty($this->_redis_ran_setup)) {
-			// Run setup on first lock.
-			//   Not in setup() for performance.
-			$this->setupRedis($Model);
-			$this->_redis_ran_setup = true;
-		}
-
-		if (!$this->settings[$Model->alias]['redis']) {
-			// Must return true so lock() tries other methods.
-			return true;
-		}
-
-		$id = trim(strval($id));
-		$cacheKey = $Model->alias . '_lock_' . $id;
-		$redisTTL = $this->settings[$Model->alias]['redisTTL'];
-
-		// BLOCKS and waits for mutex
-		$lock = $this->getRedLockObject($Model)->lock($cacheKey, $redisTTL);
-		if (empty($lock) || empty($lock['token'])) {
-			// This shouldn't ever happen. (Redis lock unable to be aquired)
-			throw new LockException('Unable to aquire transaction Lock via Redis.');
-		}
-
-		// save this lock
-		$this->locks[$Model->alias][$id] = $lock;
-
-		return true;
-	}
-
-	/**
 	 * let go of the mutex, cleanup, and carry on
 	 * delete the Redis lock key
 	 *
@@ -310,12 +192,6 @@ class LockableBehavior extends ModelBehavior {
 		if (empty($this->_currentLockMethod[$Model->alias][$id])) {
 			// Was never locked in the first place
 			return true;
-		}
-
-		if ($this->_currentLockMethod[$Model->alias][$id] == 'redis') {
-			if (!$this->unlockRedis($Model, $id)) {
-				return false;
-			}
 		}
 
 		if ($this->_currentLockMethod[$Model->alias][$id] == 'mutex') {
@@ -366,111 +242,6 @@ class LockableBehavior extends ModelBehavior {
 		unset($mutex, $this->mutexes[$Model->alias][$mutexInt]);
 
 		return true;
-	}
-
-	/**
-	 * delete the Redis lock
-	 *
-	 * @param Model $Model
-	 * @param mixed $id
-	 * @return boolean
-	 */
-	public function unlockRedis(Model $Model, $id) {
-		if (!$this->settings[$Model->alias]['redis']) {
-			// Must return true so unlock() tries other methods.
-			return true;
-		}
-
-		$id = trim(strval($id));
-		if (empty($this->locks[$Model->alias][$id])) {
-			// lock not found :(
-			return false;
-		}
-
-		// doesn't return anything, so we just assume...
-		$this->getRedLockObject($Model)->unlock($this->locks[$Model->alias][$id]);
-
-		// remove the locks Configuration
-		unset($this->locks[$Model->alias][$id]);
-
-		return true;
-	}
-
-	/**
-	 * get the RedLock object, ready for Redis Locking
-	 *
-	 * @param Model $Model
-	 * @return object $this->RedLock
-	 */
-	public function getRedLockObject(Model $Model) {
-		if (!empty($this->RedLock)) {
-			return $this->RedLock;
-		}
-
-		App::import('Lib', 'Lockable.RedLock');
-		$this->RedLock = new RedLock(
-			$this->getRedLockServers($Model),
-			$this->settings[$Model->alias]['redisRetryDelay'],
-			$this->settings[$Model->alias]['redisRetryCount']
-		);
-		return $this->RedLock;
-	}
-
-	/**
-	 * setup "servers" for RedLock
-	 *   it should support multiple servers,
-	 *   but will work ok with just one
-	 *
-	 * look for already setup/configed list in:
-	 *   $this->settings[$Model->alias]['redisServers'];
-	 *
-	 * if not found, get from Cache config
-	 *   it will use all uniquely configured Redis Cache configs
-	 *
-	 * RedLock uses a quorum, from odd numbers of servers
-	 *   but it will work find with just one server too
-	 *
-	 * @param Model $Model
-	 * @return array $servers
-	 */
-	public function getRedLockServers(Model $Model) {
-		if (!empty($this->settings[$Model->alias]['redisServers'])) {
-			return $this->settings[$Model->alias]['redisServers'];
-		}
-
-		// get config from Cache Config
-		$configs = Cache::configured();
-		$servers = [];
-		foreach ($configs as $name) {
-			$config = Cache::config($name);
-			if (empty($config['engine']) || $config['engine'] != 'Redis') {
-				continue;
-			}
-			if (empty($config['settings']['server'])) {
-				$this->log("Unable to setup Redis Lock: Cache config with RedisEngine is missing the 'server' for config {$name}");
-				continue;
-			}
-			if (empty($config['settings']['port'])) {
-				$this->log("Unable to setup Redis Lock: Cache config with RedisEngine is missing the 'port' for config {$name}");
-				continue;
-			}
-			// use a key, so we know we are not duplicating configs falsely
-			$key = $config['settings']['server'] . $config['settings']['port'];
-			$servers[$key] = array(
-				$config['settings']['server'],
-				$config['settings']['port'],
-				0.01,
-			);
-		}
-		if (empty($servers)) {
-			throw new LockException('Unable to setup Redis Lock: Cache config with RedisEngine not set up.');
-		}
-
-		// stash for future use & debugging
-		$servers = array_values($servers);
-		$this->settings[$Model->alias]['redisServers'] = $servers;
-
-		return $servers;
 	}
 
 	/**
